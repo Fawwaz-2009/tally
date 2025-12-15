64b5d1f5a19efd26f7c69e83bb9c7f59483e858a
Fawwaz <fawwaz-1427@hotmail.com>
2025-12-15 12:55:49 +0800

    remove storybook stuff

--- apps/web/.storybook/vitest.setup.ts
+++
@@ -1,5 +0,0 @@
-import { setProjectAnnotations } from '@storybook/react-vite'
-import * as projectAnnotations from './preview'
-
-// Apply Storybook configuration when testing stories
-setProjectAnnotations([projectAnnotations])
